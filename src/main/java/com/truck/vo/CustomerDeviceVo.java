000925526eb149e1952ad9111efcb3d6ce5ae622
zhangmingming <15026677294@163.com>
2018-09-28 15:48:31 +0800

    联合

---
+++ src/main/java/com/truck/vo/CustomerDeviceVo.java
@@ -0,0 +1,45 @@
+package com.truck.vo;
+
+
+public class CustomerDeviceVo {
+
+    private Integer customerId;
+
+    private Integer outDetailId;
+
+    private String model;
+
+    private String sn;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getOutDetailId() {
+        return outDetailId;
+    }
+
+    public void setOutDetailId(Integer outDetailId) {
+        this.outDetailId = outDetailId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+}
